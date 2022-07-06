5eaf567010ba15edd00cee5c30b0937468e2894e
naga <naga@nagacorp.com>
2022-07-06 15:04:49 +0530

    new code added

--- examples.py
+++ examples.py
@@ -1,3 +1,19 @@
 import streamlit as st
 value = st.slider('val')  # this is a widget
 st.write(value, 'squared is', value * value)
+
+st.title('Welcome to Streamlit!')
+
+checkbox_one = st.checkbox("Yes")
+
+checkbox_two = st.checkbox("No")
+
+if checkbox_one:
+    value = "Yes"
+
+elif checkbox_two:
+      value = "No"
+else:
+      value = "No value selected" 
+
+st.write(f"you selected: {value}")  
