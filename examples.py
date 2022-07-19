436284d7813addcb8a86bde33d808e53bd3e3835
naga <naga@nagacorp.com>
2022-07-19 15:49:04 +0530

    code change

--- examples.py
+++ examples.py
@@ -29,7 +29,19 @@ st.write(f"You selected: {select_box}")
 select_status = st.sidebar.radio("Covid-19 patient's status", ('Confirmed',
 'Active', 'Recovered', 'Deceased'))
 st.write(f"Covid-19 patient's status: {select_status}")
-picture = st.camera_input("Take a picture")
+import tensorflow as tf
 
-if picture:
-     st.image(picture)
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    # To read image file buffer as a 3D uint8 tensor with TensorFlow:
+    bytes_data = img_file_buffer.getvalue()
+    img_tensor = tf.io.decode_image(bytes_data, channels=3)
+
+    # Check the type of img_tensor:
+    # Should output: <class 'tensorflow.python.framework.ops.EagerTensor'>
+    st.write(type(img_tensor))
+
+    # Check the shape of img_tensor:
+    # Should output shape: (height, width, channels)
+    st.write(img_tensor.shape)
