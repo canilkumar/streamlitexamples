3402acc37827af1d67d7ad90eedda97f7faf5b70
naga <naga@nagacorp.com>
2022-08-04 17:18:09 +0530

    added

--- bhavani.py
+++ bhavani.py
@@ -9,3 +9,5 @@ color = st.select_slider(
      'Select a color of the rainbow',
      options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 st.write('My favorite color is', color)
+st.balloons()
+
